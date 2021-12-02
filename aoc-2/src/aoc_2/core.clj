f5ccc05f880137130c29cc60dbbc5c95ec5efdae
EdotJ <eligijus.jocius+github@gmail.com>
2021-12-02 19:16:43 +0200

    day 2 part 2 done - crude solution

--- aoc-2/src/aoc_2/core.clj
+++ aoc-2/src/aoc_2/core.clj
@@ -5,11 +5,31 @@
 (defn calc-command [command-list, command]
   (reduce + (map #(get % :value) (filter #(= (get % :command) command) command-list))))
 
-(defn main
-  []
-  (let [file-contents (slurp (io/resource "input.txt"))
-        commands (string/split file-contents #"\n")
-        command-list (map #(let [strings (string/split % #" ")] (assoc {} :command (first strings) :value (read-string (last strings)))) commands)
-        horizontal-pos (calc-command command-list "forward")
+(defn solve-first [command-list]
+  (let [horizontal-pos (calc-command command-list "forward")
         vertical-pos (- (calc-command command-list "down") (calc-command command-list "up"))]
-    (println (* horizontal-pos vertical-pos))))
+    (* horizontal-pos vertical-pos)))
+
+
+(defn get-pos [horizontal-pos, depth, aim]
+  ((* horizontal-pos (* depth aim))))
+
+(defn get-aim [prev-commands]
+  (reduce + (map #(case (get % :command)
+                    "down" (get % :value)
+                    "up" (- (get % :value))
+                    "forward" 0)
+                 prev-commands)))
+
+(defn get-depth [command-list]
+  (reduce + (filter some? (map-indexed #(when (= (get %2 :command) "forward")
+                                (let [multiplier (if (zero? (get %2 :value)) 1 (get %2 :value))] (* multiplier (get-aim (take %1 command-list)))))
+                             command-list ))))
+
+  (defn main
+    []
+    (let [file-contents (slurp (io/resource "input.txt"))
+          commands (string/split file-contents #"\n")
+          command-list (map #(let [strings (string/split % #" ")] (assoc {} :command (first strings) :value (read-string (last strings)))) commands)]
+      (println "Part One: " (solve-first command-list))
+      (println "Part Two: " (* (get-depth command-list) (calc-command command-list "forward")))))
