9985eb458d778e808f50e2569c4f3336c06c34a7
EdotJ <eligijus.jocius+github@gmail.com>
2021-12-02 19:50:07 +0200

    small cleanup

--- aoc-2/src/aoc_2/core.clj
+++ aoc-2/src/aoc_2/core.clj
@@ -2,18 +2,6 @@
   (:require [clojure.java.io :as io]
             [clojure.string :as string]))
 
-(defn calc-command [command-list, command]
-  (reduce + (map #(get % :value) (filter #(= (get % :command) command) command-list))))
-
-(defn solve-first [command-list]
-  (let [horizontal-pos (calc-command command-list "forward")
-        vertical-pos (- (calc-command command-list "down") (calc-command command-list "up"))]
-    (* horizontal-pos vertical-pos)))
-
-
-(defn get-pos [horizontal-pos, depth, aim]
-  ((* horizontal-pos (* depth aim))))
-
 (defn get-aim [prev-commands]
   (reduce + (map #(case (get % :command)
                     "down" (get % :value)
@@ -21,15 +9,30 @@
                     "forward" 0)
                  prev-commands)))
 
-(defn get-depth [command-list]
-  (reduce + (filter some? (map-indexed #(when (= (get %2 :command) "forward")
-                                (let [multiplier (if (zero? (get %2 :value)) 1 (get %2 :value))] (* multiplier (get-aim (take %1 command-list)))))
-                             command-list ))))
+(defn get-single-depth [step, prev-commands]
+  (when (= (get step :command) "forward")
+    (let [multiplier (if (zero? (get step :value)) 1 (get step :value))] (* multiplier (get-aim prev-commands)))))
 
-  (defn main
-    []
-    (let [file-contents (slurp (io/resource "input.txt"))
-          commands (string/split file-contents #"\n")
-          command-list (map #(let [strings (string/split % #" ")] (assoc {} :command (first strings) :value (read-string (last strings)))) commands)]
-      (println "Part One: " (solve-first command-list))
-      (println "Part Two: " (* (get-depth command-list) (calc-command command-list "forward")))))
+(defn get-depth [command-list]
+  (reduce + (filter some? (map-indexed #(get-single-depth %2 (take %1 command-list)) command-list))))
+
+(defn calc-step [command-list, command]
+  (reduce + (map #(get % :value) (filter #(= (get % :command) command) command-list))))
+
+(defn solve-first [command-list]
+  (let [horizontal-pos (calc-step command-list "forward")
+        vertical-pos (- (calc-step command-list "down") (calc-step command-list "up"))]
+    (* horizontal-pos vertical-pos)))
+
+(defn solve-second [command-list]
+  (let [depth (get-depth command-list)
+        horizontal-pos (calc-step command-list "forward")]
+    (* depth horizontal-pos)))
+
+(defn main
+  []
+  (let [file-contents (slurp (io/resource "input.txt"))
+        commands (string/split file-contents #"\n")
+        command-list (map #(let [strings (string/split % #" ")] (assoc {} :command (first strings) :value (read-string (last strings)))) commands)]
+    (println "Part One: " (solve-first command-list))
+    (println "Part Two: " (solve-second command-list))))
